1c11fd19c94d4525b108933738022520b65fc36d
lukdz <19475662+lukdz@users.noreply.github.com>
2026-08-22 15:16:46 +0000

    fix: use correct Ubuntu ZFS volume references

--- vm/ubuntu2410-vm.sh
+++ vm/ubuntu2410-vm.sh
@@ -581,12 +581,24 @@ nfs | dir | cifs)
   DISK_IMPORT="-format qcow2"
   THIN=""
   ;;
-btrfs | zfspool | local-zfs)
+btrfs)
   DISK_EXT=".raw"
   DISK_REF="$VMID/"
   DISK_IMPORT="-format raw"
   FORMAT=",efitype=4m"
   THIN=""
+  ;;
+zfspool | local-zfs)
+  DISK_EXT=".raw"
+  DISK_REF=""
+  DISK_IMPORT="-format raw"
+  FORMAT=",efitype=4m"
+  THIN=""
+  ;;
+*)
+  DISK_EXT=""
+  DISK_REF=""
+  DISK_IMPORT="-format raw"
   ;;
 esac
 for i in {0,1}; do
